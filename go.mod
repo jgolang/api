388f7a7a984633d58cdb0c8c2b423a65caf86129
Josue Giron <benjamingiron1@gmail.com>
2024-08-16 11:53:14 +0000

    Add id to user msg

--- go.mod
+++ go.mod
@@ -5,6 +5,10 @@ go 1.22.5
 require (
 	github.com/jgolang/errors v0.1.5
 	github.com/jgolang/helpers v0.1.1
+	go.opentelemetry.io/otel/trace v1.28.0
 )
 
-require github.com/go-errors/errors v1.5.1 // indirect
+require (
+	github.com/go-errors/errors v1.5.1 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+)
--- response_formatter.go
+++ response_formatter.go
@@ -1,12 +1,25 @@
 package api
 
-import "github.com/jgolang/api/core"
+import (
+	"fmt"
+
+	"github.com/jgolang/api/core"
+)
 
 // ResponseFormatter implementation of core.APIResponseFormatter interface.
 type ResponseFormatter struct{}
 
+// shortID returns the first 8 characters of the input string
+func shortID(s string) string {
+	if len(s) < 8 {
+		return s
+	}
+	return s[:8]
+}
+
 // Format the response body information.
 func (formatter ResponseFormatter) Format(data core.ResponseData) *core.ResponseFormatted {
+	msg := fmt.Sprintf("%s (%s-%s)", data.Message, shortID(data.EventID), data.ResponseCode)
 	return &core.ResponseFormatted{
 		HTTPStatusCode: data.HTTPStatusCode,
 		Headers:        data.Headers,
@@ -19,7 +32,7 @@ func (formatter ResponseFormatter) Format(data core.ResponseData) *core.Response
 				Action:  data.Actions,
 				Token:   data.SecurityToken,
 				Code:    string(data.ResponseCode),
-				EventID: data.EventID,
+				EventID: msg,
 				Info:    data.Info,
 			},
 		},
