62a853cd84f28035614c8ffcfde6771445018f63
Josue Giron <benjamingiron1@gmail.com>
2024-08-16 12:12:42 +0000

    Add formater validations

--- response_formatter.go
+++ response_formatter.go
@@ -9,30 +9,54 @@ import (
 // ResponseFormatter implementation of core.APIResponseFormatter interface.
 type ResponseFormatter struct{}
 
-// shortID returns the first 8 characters of the input string
-func shortID(s string) string {
-	if len(s) < 8 {
-		return s
+// TraceIDLength specifies the number of characters to return from the beginning of the input trace ID.
+var TraceIDLength = 6
+
+// shortID returns the first n characters of the input traceID
+func shortID(traceID string) string {
+	if len(traceID) < TraceIDLength {
+		return traceID
 	}
-	return s[:8]
+	return traceID[:TraceIDLength]
 }
+
+// TraceVisibility controls how trace information is included in the response message.
+// - 1: Includes both event ID and response code.
+// - 2: Includes only the event ID.
+var TraceVisibility = 1
+
+// BlankSuccess contols if success include user feeback information
+var BlankSuccess = true
 
 // Format the response body information.
 func (formatter ResponseFormatter) Format(data core.ResponseData) *core.ResponseFormatted {
-	msg := fmt.Sprintf("%s (%s-%s)", data.Message, shortID(data.EventID), data.ResponseCode)
+	msg := data.Message
+	title := data.Title
+
+	if TraceVisibility == 1 {
+		msg = fmt.Sprintf("%s (%s-%s)", data.Message, shortID(data.EventID), data.ResponseCode)
+	}
+	if TraceVisibility == 2 {
+		msg = fmt.Sprintf("%s (%s)", data.Message, shortID(data.EventID))
+	}
+	if BlankSuccess {
+		msg = ""
+		title = ""
+	}
+
 	return &core.ResponseFormatted{
 		HTTPStatusCode: data.HTTPStatusCode,
 		Headers:        data.Headers,
 		Body: JSONResponse{
 			Content: data.Content,
 			Header: JSONResponseInfo{
-				Title:   data.Title,
-				Message: data.Message,
+				Title:   title,
+				Message: msg,
 				Type:    data.ResponseType,
 				Action:  data.Actions,
 				Token:   data.SecurityToken,
 				Code:    string(data.ResponseCode),
-				EventID: msg,
+				EventID: data.EventID,
 				Info:    data.Info,
 			},
 		},
